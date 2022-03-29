26ae29a259d640e9209136d64425b76b6afdce65
UmarSiddiqui7 <the.legend4ry77@gmail.com>
2022-03-29 10:06:35 -0700

    added 2-D animation

--- PokeClone/Assets/PlayerMovement.cs
+++ PokeClone/Assets/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -17,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
