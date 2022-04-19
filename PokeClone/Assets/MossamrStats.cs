a9e4fdb238af9f97961a8bd6c00416fd5d05487d
UmarSiddiqui7 <the.legend4ry77@gmail.com>
2022-04-19 10:05:48 -0700

    Added stats.

--- PokeClone/Assets/MossamrStats.cs
+++ PokeClone/Assets/MossamrStats.cs
@@ -5,15 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class MossamrStats
 {
-    public int MaxHealth = 100;
+    public string primaryType = "Grass";
+    public string secondaryType = "Steel";
+
+    public int MaxHealth = 87;
     public int currentHealth { get; private set; }
+
+    public int maxSpeed = 87;
     public int currentSpeed { get; private set; }
-    public int maxSpeed = 80;
+
+    public int maxAttack = 87;
+    public int currentAttack { get; private set; }
+
+    public int maxSpecialAttack = 87;
+    public int currentSpecialAttack { get private set; }
+
+    public int maxDefense = 87;
+    public int currentDefense { get; private set; }
+
+    public int maxSpecialDefense = 87;
+    public int currentSpecialDefense { get; private set; }
 
     public MossamrStats()
     {
         currentHealth = MaxHealth;
         currentSpeed = maxSpeed;
+        currentAttack = maxAttack;
+        currentSpecialAttack = maxSpecialAttack;
+        currentDefense = maxDefense;
+        currentSpecialDefense = maxSpecialDefense;
+    }
+
+    public int damageDone(VolthesisStats volthesis)
+    {
+        //Damage = ((((2 * Level / 5 + 2) * AttackStat * AttackPower / DefenseStat) / 50) + 2) * STAB * Weakness/Resistance * RandomNumber / 100
+        
     }
 
     public void takeDamage(int damage, VolthesisStats volthesis)
@@ -28,19 +54,48 @@ public class MossamrStats
         }
     }
 
-    public int getSpeed()
-    {
-        return currentSpeed;
-    }
-
     public void die()
     {
-        Debug.Log("Mossamr has fainted.");
         SceneManager.LoadScene("Route 1");
     }
 
     public int getHealth()
     {
         return currentHealth;
+    }
+
+    public int getSpeed()
+    {
+        return currentSpeed;
+    }
+
+    public int getAttack()
+    {
+        return currentAttack;
+    }
+
+    public int getSpecialAttack()
+    {
+        return currentSpecialAttack;
+    }
+
+    public int getDefense()
+    {
+        return currentDefense;
+    }
+
+    public int getSpecialDefense()
+    {
+        return currentSpecialDefense;
+    }
+
+    public string getPrimaryType()
+    {
+        return primaryType;
+    }
+
+    public string getSecondaryType()
+    {
+        return secondaryType;
     }
 }
--- PokeClone/Assets/VolthesisStats.cs
+++ PokeClone/Assets/VolthesisStats.cs
@@ -5,20 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class VolthesisStats
 {
-    public int MaxHealth = 100;
+    public string primaryType = "Fire";
+    public string secondaryType = "Fairy";
+
+    public int MaxHealth = 87;
     public int currentHealth { get; private set; }
+    
+    public int maxSpeed = 87;
     public int currentSpeed { get; private set; }
-    public int maxSpeed = 40;
+
+    public int maxAttack = 87;
+    public int currentAttack { get; private set; }
+
+    public int maxSpecialAttack = 87;
+    public int currentSpecialAttack { get private set; }
+
+    public int maxDefense = 87;
+    public int currentDefense { get; private set; }
+
+    public int maxSpecialDefense = 87;
+    public int currentSpecialDefense { get; private set; }
 
     public VolthesisStats()
     {
         currentHealth = MaxHealth;
         currentSpeed = maxSpeed;
+        currentAttack = maxAttack;
+        currentSpecialAttack = maxSpecialAttack;
+        currentDefense = maxDefense;
+        currentSpecialDefense = maxSpecialDefense;
     }
 
-    public int getSpeed()
+    public int damageDone(MossamrStats mossamr)
     {
-        return currentSpeed;
+
     }
 
     public void takeDamage(int damage, MossamrStats mossamr)
@@ -43,5 +63,40 @@ public class VolthesisStats
     public int getHealth()
     {
         return currentHealth;
+    }
+    
+    public int getSpeed()
+    {
+        return currentSpeed;
+    }
+
+    public int getAttack()
+    {
+        return currentAttack;
+    }
+
+    public int getSpecialAttack()
+    {
+        return currentSpecialAttack;
+    }
+
+    public int getDefense()
+    {
+        return currentDefense;
+    }
+
+    public int getSpecialDefense()
+    {
+        return currentSpecialDefense;
+    }
+
+    public string getPrimaryType()
+    {
+        return primaryType;
+    }
+
+    public string getSecondaryType()
+    {
+        return secondaryType;
     }
 }
