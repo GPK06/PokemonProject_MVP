68b243e9dd75977e3d5edb1a265cda7b96e7fe96
UmarSiddiqui7 <the.legend4ry77@gmail.com>
2022-05-20 10:08:39 -0700

    Started to refactor

---
+++ PokeClone/Assets/Scripts/BattleOptions.cs
@@ -0,0 +1,221 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BattleOptions : MonoBehaviour
+{
+    PokemonStats mossamr;
+    PokemonStats volthesis;
+    PokemonStats wargo;
+
+    HashSet<string> weaknessMossamr = new HashSet<string>();
+    HashSet<string> resistanceMossamr = new HashSet<string>();
+    HashSet<string> immunityMossamr = new HashSet<string>();
+
+    HashSet<string> weaknessVolthesis = new HashSet<string>();
+    HashSet<string> resistanceVolthesis = new HashSet<string>();
+    HashSet<string> immunityVolthesis = new HashSet<string>();
+
+    HashSet<string> weaknessWargo = new HashSet<string>();
+    HashSet<string> resistanceWargo = new HashSet<string>();
+
+    public BattleOptions()
+    {
+        addWeaknessMossamr();
+        addWeaknessVolthesis();
+        addWeaknessWargo();
+        
+        addResistanceWargo();
+        addResistanceVolthesis();
+        addResistanceMossamr();
+
+        addImmunityVolthesis();
+        addImmunityMossamr();
+
+        //public PokemonStats(string name, string primary, string secondary, int health, int speed, int attack, int spAttack, int defense, int spDefense, HashSet<string> weaknessP, HashSet<string> resistanceP, HashSet<string> immunityP)
+        mossamr = new PokemonStats("Mossamr", "grass", "steel", 78, 75, 120, 41, 136, 80, weaknessMossamr, resistanceMossamr, immunityMossamr);
+        volthesis = new PokemonStats("Volthesis", "fire", "fairy", 110, 21, 55, 143, 129, 72, weaknessVolthesis, resistanceVolthesis, immunityVolthesis);
+        wargo = new PokemonStats("Wargo", "water", "dragon", 54, 88, 104, 104, 90, 90, weaknessWargo, resistanceWargo, null);
+    }
+
+    public void runAway()
+    {
+        SceneManager.LoadScene("Route 1");
+    }
+
+    public void battle(int n)
+    {
+        Text totalHealth;
+        totalHealth = transform.Find("TotalHealth").GetComponent<Text>();
+        totalHealth.text = volthesis.getHealth() + "";
+
+        Text totalHealthMossamr;
+        totalHealthMossamr = transform.Find("TotalHealthMossamr").GetComponent<Text>();
+        totalHealthMossamr.text = mossamr.getHealth() + "";
+
+        string stat;
+
+        double mossamrDamage;
+        double volthDamage;
+        double mossamrFrac;
+        double volthFrac;
+
+        volthFrac = volthesis.effectiveness(mossamr.getPrimaryType());
+        double mossamrDamagePrimary = wargo.damageDone(volthesis, mossamr.getPrimaryType(), volthFrac);
+        volthFrac = volthesis.effectiveness(mossamr.getSecondaryType());
+        double mossamrDamageSecondary = wargo.damageDone(volthesis, mossamr.getSecondaryType(), volthFrac);
+
+        if (mossamrDamagePrimary > mossamrDamageSecondary)
+        {
+            mossamrDamage = mossamrDamagePrimary;
+        }
+        else
+        {
+            mossamrDamage = mossamrDamageSecondary;
+        }
+
+        if (n == 1)
+        {
+            // if int n is a 1 then we are using the primary stat
+            mossamrFrac = mossamr.effectiveness(volthesis.getPrimaryType());
+            volthDamage = volthesis.damageDone(mossamr, volthesis.getPrimaryType(), mossamrFrac);
+        }
+        else
+        {
+            // if int n is a 2 then we are using the secondary stat
+            mossamrFrac = mossamr.effectiveness(volthesis.getSecondaryType());
+            volthDamage = volthesis.damageDone(mossamr, volthesis.getSecondaryType(), mossamrFrac);
+        }
+
+        if (volthesis.getSpeed() > wargo.getSpeed())
+        {
+            mossamr.takeDamage(volthDamage, volthesis);
+            volthesis.takeDamage(mossamrDamage, mossamr);
+        }
+        else
+        {
+            volthesis.takeDamage(mossamrDamage, mossamr);
+            mossamr.takeDamage(volthDamage, volthesis);
+        }
+    }
+
+    public void battleWargo(int n)
+    {
+        string stat;
+
+        double wargoDamage;
+        double volthDamage;
+        double wargoFrac;
+        double volthFrac;
+
+        volthFrac = volthesis.effectiveness(wargo.getPrimaryType());
+        double wargoDamagePrimary = wargo.damageDone(volthesis, wargo.getPrimaryType(), volthFrac);
+        volthFrac = volthesis.effectiveness(wargo.getSecondaryType());
+        double wargoDamageSecondary = wargo.damageDone(volthesis, wargo.getSecondaryType(), volthFrac);
+
+        if (wargoDamagePrimary > wargoDamageSecondary)
+        {
+            wargoDamage = wargoDamagePrimary;
+        } else
+        {
+            wargoDamage = wargoDamageSecondary;
+        }
+
+        if (n == 1)
+        {
+            // if int n is a 1 then we are using the primary stat
+            wargoFrac = wargo.effectiveness(volthesis.getPrimaryType());
+            volthDamage = volthesis.damageDone(wargo, volthesis.getPrimaryType(), wargoFrac);
+        }
+        else
+        {
+            // if int n is a 2 then we are using the secondary stat
+            wargoFrac = wargo.effectiveness(volthesis.getSecondaryType());
+            volthDamage = volthesis.damageDone(wargo, volthesis.getSecondaryType(), wargoFrac);
+        }
+
+        if (volthesis.getSpeed() > wargo.getSpeed())
+        {
+            wargo.takeDamage(volthDamage, volthesis);
+            volthesis.takeDamage(wargoDamage, wargo);
+        }
+        else
+        {
+            volthesis.takeDamage(wargoDamage, wargo);
+            wargo.takeDamage(volthDamage, volthesis);
+        }
+    }
+
+    public void moveSelection()
+    {
+        SceneManager.LoadScene("MoveSelection");
+    }
+    
+    public void moveSelectionWargo()
+    {
+        SceneManager.LoadScene("MoveSelectionWargo");
+    }
+
+    public void addWeaknessMossamr()
+    {
+        weaknessMossamr.Add("fire 2"); //4x weakness
+        weaknessMossamr.Add("fighting");
+    }
+
+    public void addResistanceMossamr()
+    {
+        resistanceMossamr.Add("normal");
+        resistanceMossamr.Add("grass 2"); // 4x weakness
+        resistanceMossamr.Add("water");
+        resistanceMossamr.Add("electric");
+        resistanceMossamr.Add("psychic");
+        resistanceMossamr.Add("rock");
+        resistanceMossamr.Add("dragon");
+        resistanceMossamr.Add("steel");
+        resistanceMossamr.Add("fairy");
+    }
+
+    public void addImmunityMossamr()
+    {
+        immunityMossamr.Add("poison");
+    }
+
+    public void addWeaknessVolthesis()
+    {
+        weaknessVolthesis.Add("water");
+        weaknessVolthesis.Add("ground");
+        weaknessVolthesis.Add("poison");
+        weaknessVolthesis.Add("rock");
+    }
+
+    public void addResistanceVolthesis()
+    {
+        resistanceVolthesis.Add("fire");
+        resistanceVolthesis.Add("grass");
+        resistanceVolthesis.Add("ice");
+        resistanceVolthesis.Add("fighting");
+        resistanceVolthesis.Add("fairy");
+        resistanceVolthesis.Add("bug 2"); // 4x resistance
+        resistanceVolthesis.Add("dark");
+    }
+
+    public void addImmunityVolthesis()
+    {
+        immunityVolthesis.Add("dragon");
+    }
+
+    public void addWeaknessWargo()
+    {
+        weaknessWargo.Add("fairy"); //4x weakness
+        weaknessWargo.Add("dragon");
+    }
+
+    public void addResistanceWargo()
+    {
+        resistanceWargo.Add("fire 2");
+        resistanceWargo.Add("water 2");
+        resistanceWargo.Add("steel");
+    }
+}
---
+++ PokeClone/Assets/Scripts/PokemonStats.cs
@@ -0,0 +1,216 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PokemonStats : MonoBehaviour
+{
+    public string name;
+
+    public string primaryType;
+    public string secondaryType;
+
+    public int MaxHealth;
+    public double currentHealth;
+
+    public int MaxSpeed;
+    public int currentSpeed;
+
+    public int MaxAttack;
+    public int currentAttack;
+
+    public int MaxSpecialAttack;
+    public int currentSpecialAttack;
+
+    public int MaxDefense;
+    public int currentDefense;
+
+    public int MaxSpecialDefense;
+    public int currentSpecialDefense;
+
+    public Move primaryMove;
+    public Move secondaryMove;
+
+    public HashSet<string> weakness;
+    public HashSet<string> resistance;
+    public HashSet<string> immunity;
+
+    public PokemonStats(string name, string primary, string secondary, int health, int speed, int attack, int spAttack, int defense, int spDefense, HashSet<string> weaknessP, HashSet<string> resistanceP, HashSet<string> immunityP)
+    {
+        this.name = name;
+        
+        primaryType = primary;
+        secondaryType = secondary;
+
+        MaxHealth = health;
+        MaxSpeed= speed;
+        MaxAttack = attack;
+        MaxSpecialAttack = spAttack;
+        MaxDefense = defense;
+        MaxSpecialDefense = spDefense;
+
+        currentHealth = MaxHealth;
+        currentSpeed = MaxSpeed;
+        currentSpecialAttack = MaxSpecialAttack;
+        currentAttack = MaxAttack;
+        currentSpecialDefense = MaxSpecialDefense;
+        currentDefense = MaxDefense;
+
+        weakness = weaknessP;
+        resistance = resistanceP;
+        immunity = immunityP;
+
+        primaryMove = new Move(80, "Attack", primaryType);
+        secondaryMove = new Move(80, "Attack", secondaryType);
+    }
+
+    public double damageDone(PokemonStats pokemon, string typeBeingUsed, double effective)
+    {
+        //Damage = ((((2 * Level / 5 + 2) * AttackStat * AttackPower / DefenseStat) / 50) + 2) * STAB * Weakness/Resistance * RandomNumber / 100
+        //Level will just be 50 before a level up system is added.
+
+        //double effective = effectiveness(typeBeingUsed);
+
+        int attackPower;
+        Move type;
+        if (primaryMove.getType().Equals(typeBeingUsed))
+        {
+            type = primaryMove;
+            attackPower = primaryMove.getBasePower();
+        }
+        else
+        {
+            type = secondaryMove;
+            attackPower = secondaryMove.getBasePower();
+        }
+
+        int attackStat;
+        int defenseStat;
+
+        if (type.getType().Equals("SpecialAttack"))
+        {
+            attackStat = getSpecialAttack(); // will have to use the move.getAttackStatBeingUsed
+            defenseStat = pokemon.getSpecialDefense();
+        }
+        else
+        {
+            attackStat = getAttack();
+            defenseStat = pokemon.getDefense();
+        }
+
+        double randomNum = Random.Range(85, 101);
+        randomNum /= 100;
+
+        //Debug.Log("Stats: Mossamr" + effective);
+
+        double damage = ((((2 * 50 / 5 + 2) * attackStat * attackPower / defenseStat) / 50) + 2) * 1.5 * effective * randomNum;
+        return damage;
+    }
+
+    public double effectiveness(string type)
+    {
+        int numerator = 1;
+        int denominator = 1;
+        int stringReturnVal = 0;
+
+        if (immunity != null)
+        {
+            if (immunity.Contains(type))
+            {
+                return 0;
+            }
+        }
+
+        foreach (string weaknessType in weakness)
+        {
+            if (weaknessType.Contains(type))
+            {
+                numerator = 2;
+
+                if (weaknessType.Contains("2"))
+                {
+                    numerator = 4;
+                }
+            }
+        }
+
+        foreach (string resistanceType in resistance)
+        {
+            if (resistanceType.Contains(type))
+            {
+                denominator = 2;
+                if (resistanceType.Contains(type))
+                {
+                    denominator = 4;
+                }
+            }
+        }
+
+        double returnVal = numerator;
+        returnVal /= denominator;
+
+        return returnVal;
+    }
+
+    public void takeDamage(double damage, PokemonStats pokemon)
+    {
+        currentHealth -= damage;
+
+        Text healthText;
+        //levelText = transform.Find(“levelText”).GetComponent<Text>();
+        healthText = transform.Find("currentHealth").GetComponent<Text>();
+        healthText.text = currentHealth + "";
+
+        if (currentHealth <= 0 && pokemon.getHealth() != 0)
+        {
+            die();
+        }
+    }
+
+    public void die()
+    {
+        SceneManager.LoadScene("Route 1");
+    }
+
+    public double getHealth()
+    {
+        return currentHealth;
+    }
+
+    public int getSpeed()
+    {
+        return currentSpeed;
+    }
+
+    public int getAttack()
+    {
+        return currentAttack;
+    }
+
+    public int getSpecialAttack()
+    {
+        return currentSpecialAttack;
+    }
+
+    public int getDefense()
+    {
+        return currentDefense;
+    }
+
+    public int getSpecialDefense()
+    {
+        return currentSpecialDefense;
+    }
+
+    public string getPrimaryType()
+    {
+        return primaryType;
+    }
+
+    public string getSecondaryType()
+    {
+        return secondaryType;
+    }
+}
